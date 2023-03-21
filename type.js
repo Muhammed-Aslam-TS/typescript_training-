550fcd6afb5d33c5c6383c429f272f182f073b55
muhammed aslam <taslam8797@gmail.com>
2023-03-21 12:41:40 +0530

    interface

--- type.js
+++ type.js
@@ -1,11 +1,26 @@
 "use strict";
-class person {
-    constructor(name) {
-        this.name = name;
-    }
-    get() {
-        return this.name;
-    }
-}
-let p1 = new person('jone');
-console.log(p1.get());
+//class concepts
+// class person {
+//     private readonly name:string
+//     constructor(name:string){
+//         this.name = name
+//     }
+//     get():string{
+//         return this.name
+//     }
+// }
+// let p1= new person('jone')
+// console.log(p1.get());
+//inheritance___implement
+// interface school {
+//     getArea:()=> number;
+// }
+// class student implements school {
+//     constructor(protected readonly fname: string,protected readonly lname:string){
+//     }
+//     getArea():any{
+//         return this.fname+' '+ this.lname;
+//     }
+// }
+// let s1 = new student('muhammed','aslam')
+// console.log(s1.getArea());
--- type.ts
+++ type.ts
@@ -1,16 +1,38 @@
+//class concepts
+// class person {
+//     private readonly name:string
+//     constructor(name:string){
+//         this.name = name
+//     }
 
-class person {
-    private readonly name:string
-    constructor(name:string){
-        this.name = name
-    }
+//     get():string{
+//         return this.name
+//     }
+// }
 
-    get():string{
-        return this.name
-    }
-}
+// let p1= new person('jone')
+// console.log(p1.get());
 
-let p1= new person('jone')
-console.log(p1.get());
+
+
+
+//inheritance___implement
+
+
+// interface school {
+//     getArea:()=> number;
+// }
+
+// class student implements school {
+//     constructor(protected readonly fname: string,protected readonly lname:string){
+
+//     }
+//     getArea():any{
+//         return this.fname+' '+ this.lname;
+//     }
+// }
+
+// let s1 = new student('muhammed','aslam')
+// console.log(s1.getArea());
 
 
